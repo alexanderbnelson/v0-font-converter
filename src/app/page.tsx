db5e47268ce81f3fd09b62e49465ed62b9ef3292
Alex Nelson <alex@postprojects.com>
2025-08-27 10:48:45 -0700

    update tw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import FontConverter from "./components/FontConverter";
 
 export default function Home() {
   return (
-    <div className="container mx-auto py-8 font-[family-name:var(--font-geist-sans)] h-full flex items-center justify-center">
+    <div className="container mx-auto py-8 font-(family-name:--font-geist-sans) h-full flex items-center justify-center">
       <Card className="p-10 max-w-xl w-full mx-auto">
         <h1 className="text-3xl font-bold mb-6">Font Converter</h1>
         <FontConverter />
